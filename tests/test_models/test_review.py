dabca4af78e74d670282476bb9183e4874dff007
Igomigo <igomigofatai@gmail.com>
2023-05-14 04:18:17 -0700

    Test cases update

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""The Unittest module that contains the Review Class test cases."""
+
+import unittest
+from datetime import datetime
+import time
+from models.review import Review
+import re
+import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
+from models.base_model import BaseModel
+
+
+class TestReview(unittest.TestCase):
+    """Test Cases for the Review class."""
+
+    def setUp(self):
+        """Sets up test methods."""
+        pass
+
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_8_instantiation(self):
+        """Tests instantiation of Review class."""
+
+        r = Review()
+        self.assertEqual(str(type(r)), "<class 'models.review.Review'>")
+        self.assertIsInstance(r, Review)
+        self.assertTrue(issubclass(type(r), BaseModel))
+
+    def test_8_attributes(self):
+        """Tests the attributes of Review class."""
+        attributes = storage.attributes()["Review"]
+        o = Review()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
+
+if __name__ == "__main__":
+    unittest.main()
